8bf48e8e2e36c7cec03d68990138501e7bb01d1b
Mabarik667f <jc.bob1412@gmail.com>
2024-05-02 12:21:30 +0300

    create first functions registry, work in products and create order

--- Dockerfile
+++ Dockerfile
@@ -6,5 +6,7 @@ ADD src/protobuf/common_pb2.py /protobuf/
 ADD src/protobuf/contract_pb2.py /protobuf/
 ADD src/protobuf/contract_pb2_grpc.py /protobuf/
 ADD run.sh /
+ADD contract_handler.log /contract_handler.log
+
 RUN chmod +x run.sh
 ENTRYPOINT ["/run.sh"]
--- src/contract.py
+++ src/contract.py
@@ -1,6 +1,14 @@
 import grpc
 import os
 import sys
+import logging
+
+logging.basicConfig(
+    filename='contract_handler.log',  # Укажите имя файла, куда будут записываться логи
+    level=logging.INFO,  # Уровень логгирования (INFO, DEBUG, ERROR и т. д.)
+    format='%(asctime)s - %(levelname)s - %(message)s'  # Формат записи логов
+)
+
 
 from protobuf import common_pb2, contract_pb2, contract_pb2_grpc
 
@@ -42,9 +50,11 @@ class ContractHandler:
         create_transaction = contract_transaction_response.transaction
         request = contract_pb2.ExecutionSuccessRequest(  # возвращаем успешный ответ
             tx_id=create_transaction.id,  # id транзакции
-            results=[common_pb2.DataEntry(  # сохранение данных в блокчейн
-                    key="sum",
-                    int_value=0)]
+            results=[common_pb2.DataEntry(key="users", int_value=0),
+                     common_pb2.DataEntry(key="makers", int_value=0),
+                     common_pb2.DataEntry(key="distributors", int_value=0),
+                     common_pb2.DataEntry(key="products", int_value=0),
+                     common_pb2.DataEntry(key="orders", int_value=0)]
         )
         metadata = [(AUTH_METADATA_KEY, contract_transaction_response.auth_token)]
         response = self.client.CommitExecutionSuccess(request=request, metadata=metadata)  # ответ, сохранение данных
@@ -54,80 +64,250 @@ class ContractHandler:
         call_transaction = contract_transaction_response.transaction
         metadata = [(AUTH_METADATA_KEY, contract_transaction_response.auth_token)]
 
-        # k = self.client.GetContractKey(call_transaction, "key")
-        contract_key_request = contract_pb2.ContractKeyRequest(
-            contract_id=call_transaction.contract_id,
-            key="sum"
-        )
-        # получаем ключ, из которого можно взять данные
-        contract_key = self.client.GetContractKey(request=contract_key_request, metadata=metadata)
-        old_value = contract_key.entry.int_value # получаем данные по ключу
+        data = self.client_registry(call_transaction, metadata)
+        # match tx_type:
+        #     case "client_registry":
+        #         request = self.client_registry(call_transaction, metadata)
+        #     case "maker_registry":
+        #         request = self.maker_registry(call_transaction, metadata)
+        #     case "distributor_registry":
+        #         request = self.distributor_registry(call_transaction, metadata)
+        #     case "review_register":
+        #         request = self.review_register(call_transaction, metadata)
+        #     case "create_product":
+        #         request = self.create_product(call_transaction, metadata)
+        #     case "review_product":
+        #         request = self.review_product(call_transaction, metadata)
+        #     case "change_product":
+        #         request = self.change_product(call_transaction, metadata)
+        #     case "create_order":
+        #         request = self.create_product(call_transaction, metadata)
+        #     case "review_order":
+        #         request = self.review_order(call_transaction, metadata)
+        #     case "accept_order":
+        #         request = self.accept_order(call_transaction, metadata)
+        #     case "cancel_order":
+        #         request = self.cancel_order(call_transaction, metadata)
+        #     case "get_user":
+        #         pass
+        #     case "get_all_user":
+        #         pass
+        #     case "get_product":
+        #         pass
+        #     case "get_all_products":
+        #         pass
+        #     case "get_order":
+        #         pass
+        #     case "get_all_orders":
+        #         pass
 
-        request = contract_pb2.ExecutionSuccessRequest(  # возвращаем успешный ответ
-            tx_id=call_transaction.id,  # id транзакции
-            results=[common_pb2.DataEntry(  # сохранение данных в блокчейн
-                key="sum",
-                int_value=old_value + 10)]
+        request = contract_pb2.ExecutionSuccessRequest(
+            tx_id=call_transaction.id,
+            results=data
         )
+
         response = self.client.CommitExecutionSuccess(request=request, metadata=metadata)
         print("in call tx response '{}'".format(response))
 
+    @staticmethod
+    def get_string_value(call_transaction, key):
+        res = ''
+        for param in call_transaction.params:
+            if param.key == key:
+                res = param.str_value
+                break
+        return res
+
+    @staticmethod
+    def get_int_value(call_transaction, key):
+        res = 0
+        for param in call_transaction.params:
+            if param.key == key:
+                res = param.int_value
+                break
+        return res
+
+    @staticmethod
+    def get_bool_value(call_transaction, key):
+        res = False
+        for param in call_transaction.params:
+            if param.key == key:
+                res = param.bool_value
+                break
+        return res
+
     # Блок регистации
     def client_registry(self, call_transaction, metadata):
-        pass
+        # получаем ключ, из которого можно взять данные
+
+        try:
+            contract_key_request = contract_pb2.ContractKeyRequest(
+                contract_id=call_transaction.contract_id,
+                key="users"
+            )
+            contract_key = self.client.GetContractKey(request=contract_key_request, metadata=metadata)
+            user_id = contract_key.entry.int_value
+
+            data = [
+                common_pb2.DataEntry(key=f"request_user_{user_id + 1}",
+                                     str_value=self.get_string_value(call_transaction, "user")),
+                common_pb2.DataEntry(key="users", int_value=user_id + 1)
+            ]
+
+            return data
+        except Exception as error:
+            logging.info(f'{error}')
 
     def maker_registry(self, call_transaction, metadata):
-        pass
+        try:
+            contract_key_request = contract_pb2.ContractKeyRequest(
+                contract_id=call_transaction.contract_id,
+                key="makers"
+            )
+            contract_key = self.client.GetContractKey(request=contract_key_request, metadata=metadata)
+            maker_id = contract_key.entry.int_value
+
+            data = [
+                common_pb2.DataEntry(key=f"request_maker_{maker_id + 1}",
+                                     str_value=self.get_string_value(call_transaction, "maker")),
+                common_pb2.DataEntry(key="makers", int_value=maker_id + 1)
+            ]
+
+            return data
+        except Exception as error:
+            logging.info(f'{error}')
 
     def distributor_registry(self, call_transaction, metadata):
-        pass
+        try:
+            contract_key_request = contract_pb2.ContractKeyRequest(
+                contract_id=call_transaction.contract_id,
+                key="distributors"
+            )
+            contract_key = self.client.GetContractKey(request=contract_key_request, metadata=metadata)
+            distributor_id = contract_key.entry.int_value
 
-    def review_register(self, call_transaction, metadata):
-        pass
+            data = [
+                common_pb2.DataEntry(key=f"request_distributor_{distributor_id + 1}",
+                                     str_value=self.get_string_value(call_transaction, "distributor")),
+                common_pb2.DataEntry(key="distributors", int_value=distributor_id + 1)
+            ]
+
+            return data
+        except Exception as error:
+            logging.info(f'{error}')
+
+    def review_register(self, call_transaction, metadata): # !!!!!!!!!!
+        """одобряем регистрацию"""
+
+        distributor_id = self.get_int_value(call_transaction, key="distributor")
+        maker_id = self.get_int_value(call_transaction, key="maker")
+        if distributor_id != 0:
+            get_key = f"request_distributor_{distributor_id}"
+        elif maker_id != 0:
+            get_key = f"request_maker_{maker_id}"
+        else:
+            user_id = self.get_int_value(call_transaction, key="user")
+            get_key = f"request_user_{user_id}"
+
+        contract_key_request = contract_pb2.ContractKeyRequest(
+            contract_id=call_transaction.contract_id,
+            key=get_key
+        )
+
+        contract_key = self.client.GetContractKey(request=contract_key_request, metadata=metadata)
+        _id = contract_key.entry.str_value
+
+        data = [
+            common_pb2.DataEntry(key=get_key,str_value=""), # меняем флаг на true
+        ]
+
+        return data
 
     # Блок создание нового товара
     def create_product(self, call_transaction, metadata):
-        pass
+        try:
+            contract_key_request = contract_pb2.ContractKeyRequest(
+                contract_id=call_transaction.contract_id,
+                key="products"
+            )
+            contract_key = self.client.GetContractKey(request=contract_key_request, metadata=metadata)
+            product_id = contract_key.entry.int_value
 
-    def change_product(self, call_transaction, metadata):
-        pass
+            data = [
+                common_pb2.DataEntry(key=f"product_{product_id + 1}",
+                                     str_value=self.get_string_value(call_transaction, "product")),
+                common_pb2.DataEntry(key="products", int_value=product_id + 1)
+            ]
 
-    def review_product(self, call_transaction, metadata):
-        pass
+            return data
+        except Exception as error:
+            logging.info(f'{error}')
+
+    def review_product(self, call_transaction, metadata): # !!!!!!!!!!
+        """Добавляем данные в продукт и публикуем его"""
+        product_id = self.get_int_value(call_transaction, key="product")
+        contract_key_request = contract_pb2.ContractKeyRequest(
+            contract_id=call_transaction.contract_id,
+            key=f"product_{product_id}"
+        )
+
+        contract_key = self.client.GetContractKey(request=contract_key_request, metadata=metadata)
+        product = contract_key.entry.str_value
+
+        data = [
+            common_pb2.DataEntry(key=f"product_{product_id}",
+                                 value="product") # работаем со значением
+        ]
+
+        return data
 
     # Блок создания заказа
 
     def create_order(self, call_transaction, metadata):
-        pass
+        try:
+            contract_key_request = contract_pb2.ContractKeyRequest(
+                contract_id=call_transaction.contract_id,
+                key="orders"
+            )
+            contract_key = self.client.GetContractKey(request=contract_key_request, metadata=metadata)
+            order_id = contract_key.entry.int_value
+
+            data = [
+                common_pb2.DataEntry(key=f"request_product_{order_id + 1}",
+                                     str_value=self.get_string_value(call_transaction, "order")),
+                common_pb2.DataEntry(key="orders", int_value=order_id + 1)
+            ]
+
+            return data
+        except Exception as error:
+            logging.info(f'{error}')
 
     def review_order(self, call_transaction, metadata):
         pass
 
-    def accept_order(self, call_transaction, metadata):
+    def user_order_review(self, call_transaction, metadata):
         pass
 
-    def cancel_order(self, call_transaction, metadata):
-        pass
 
     # Блок получения данных
 
-    def get_user(self, call_transaction, metadata):
-        pass
-
-    def get_all_user(self, call_transaction, metadata):
-        pass
-
-    def get_product(self, call_transaction, metadata):
-        pass
-
-    def get_all_products(self, call_transaction, metadata):
-        pass
-
-    def get_order(self, call_transaction, metadata):
-        pass
-
-    def get_all_orders(self, call_transaction, metadata):
-        pass
+    # def get_user(self, call_transaction, metadata):
+    #     pass
+    #
+    # def get_all_user(self, call_transaction, metadata):
+    #     pass
+    #
+    # def get_product(self, call_transaction, metadata):
+    #     pass
+    #
+    # def get_all_products(self, call_transaction, metadata):
+    #     pass
+    #
+    # def get_order(self, call_transaction, metadata):
+    #     pass
+    #
+    # def get_all_orders(self, call_transaction, metadata):
+    #     pass
 
 
 def run(connection_id, node_host, node_port, connection_token):
